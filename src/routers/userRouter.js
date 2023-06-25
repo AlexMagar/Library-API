5670ddba8f385bd75a075352cad4fc688fdefd48
AlexMagar <lx.mgr07@gmail.com>
2023-06-25 22:48:37 +1000

    route the get, post request to db

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -6,7 +6,6 @@ const router = express.Router();
 
 router.get("/", (req, res) =>{
     try {
-        console.log(req.body)
         res.json({
             status: "success",
             message: "Here are the user information"
@@ -22,7 +21,7 @@ router.get("/", (req, res) =>{
 
 router.post("/", async (req, res) =>{
     try {
-        
+
         const {password} = req.body;
         req.body.password = hashPassword(password);
 
