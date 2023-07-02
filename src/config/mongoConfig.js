8afae45103fbeaee557b5535e6e284bdc1867ac1
AlexMagar <lx.mgr07@gmail.com>
2023-07-02 16:04:46 +1000

    changes

--- src/config/mongoConfig.js
+++ src/config/mongoConfig.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectMongoDB = async () =>{
     try {
         const conn = await mongoose.connect(process.env.MONGO_CLIENT);
-        conn && console.log("Mongo Connected") // if conn is true then console.log()
+        conn && console.log("Mongo Connected") // if conn is true then console.log()...
     } catch (error) {
         console.log(error)
     }
