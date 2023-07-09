d83aaf01e6cc7c2640f0cf2d9289894d704701c5
AlexMagar <lx.mgr07@gmail.com>
2023-07-09 20:37:00 +1000

    auth middle ware added for authenticate and authorize

---
+++ src/middleware/authMiddleware.js
@@ -0,0 +1,50 @@
+import { getUserById } from "../modles/user/UserModel.js";
+
+export const auth = async (req, res, next) => {
+    try {
+        //every request have userId
+        const {authorization} = req.headers;
+
+        // get the user from database
+        const user = await getUserById(authorization);
+
+        if(user?._id){
+            // check the role
+            user.password = undefined;
+
+            req.userInfo = user;
+            //let it go to next router
+            return next()
+        }
+
+        //stop here and response to client 
+        res.json({
+            status: "error",
+            message: "Sorry you dont have permission to this API"
+        })
+    } catch (err) {
+        res.json({
+            status: "error",
+            message: err.message,
+        })
+    }
+}
+
+export const adminAuth = async (req, res, next) => {
+    try {
+        const {role} = req.userInfo;
+        console.log(role)
+
+        role === "admin"
+            ? next()
+            : res.json({
+                status: "error",
+                message: "Not allowed because you are not admin"
+            })
+    } catch (error) {
+        res.json({
+            status: "error",
+            message: error.message,
+        })
+    }
+}
